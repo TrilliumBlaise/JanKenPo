ea5d261657cb42eceebc544c51c106ccacdc141a
TrilliumBlaise <79470625+TrilliumBlaise@users.noreply.github.com>
2022-04-19 11:31:49 -0400

    Add files via upload

---
+++ src/main/webapp/frontend/instructions.js
@@ -0,0 +1,19 @@
+export const instructions = [
+  `Hello, you are about to embark on an epic battle to control the pond! During the course of this fight, you will encounter a wide range of
+    enemy types, from algae to snakes.`,
+  `You and your enemy will have 3 stats: <br />
+    HP is the amount of damage that can be taken. <br />
+    Power is the strength of an attack. <br />
+    Defense is the ability to withstand damage <br />`,
+  `During each fight, you will be asked to click on either a Rock, a Paper, or a Scissors icon. Doing so will input what your attack type is.
+    <br />
+    Rock beats Scissors.<br />
+    Paper beats Rock.<br />
+    Scissors beats Paper.`,
+  `If you win the fight, you will inflict damage on your opponent. If your oopponent wins, you will receive damage. The battle will continue
+    until either you or your enemy reaches 0 HP.`,
+  `The damage dealt to the loser is calculated by the winner's power and the loser's defense (power - defense).<br />
+    If the loser's defense is >= winner's power, then the damage will equal 5.`,
+  `Each time you defeat an opponent, you will have a selection of rewards to choose from. If you are defeated, you will have to start over.`,
+  `Do you have what it takes to come out on top?`,
+];

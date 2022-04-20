e67bca9e4dc26056a4ffb2cc5eb40ae5dd3d9d3d
TrilliumBlaise <79470625+TrilliumBlaise@users.noreply.github.com>
2022-04-20 09:18:24 -0400

    Add files via upload

---
+++ src/main/webapp/backend/enemies.js
@@ -0,0 +1,14 @@
+export const enemies = [
+  {
+    name: ['Snail', 'Tadpole', 'Algea'],
+    stats: [50, 15, 0],
+  },
+  {
+    name: ['Dragon', 'Worm', 'Spider'],
+    stats: [70, 15, 5],
+  },
+  {
+    name: ['Frog', 'Fish', 'Snake'],
+    stats: [80, 30, 10],
+  },
+];
